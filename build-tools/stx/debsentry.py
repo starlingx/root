5c799e96fb37ef41ace8a6e8079ad1685416eee8
Yue Tao <yue.tao@windriver.com>
2022-04-28 16:31:11 +0800

    debsentry.py: capture exceptions
    The manipulation of file, and pickle load/dump doesn't run in try
    block. An empty or an invalid pickle file can crash the build system.
    
    The crash of an empty file is:
    
    File "/localdisk/designer/ytao/stx/cgcs-root/build-tools/stx/build-pkgs", line 412, in upload_with_deb
      subdebs = debsentry.get_subdebs(debs_clue, package, logger)
    File "/localdisk/designer/ytao/stx/cgcs-root/build-tools/stx/debsentry.py", line 24, in get_subdebs
      debs = pickle.load(fclue)
    EOFError: Ran out of input
    
    Test Plan:
    
    Pass: build-pkgs -c -p package with an empty debs_entry.pkl file
    Pass: build-pkgs -c -p package with an invalid debs_entry.pkl file
    
    Story: 2008846
    Task: 45205
    
    Signed-off-by: Yue Tao <yue.tao@windriver.com>
    Change-Id: Id9ee74c1f9be09e759926e3031a4a9a01861db37

--- build-tools/stx/debsentry.py
+++ build-tools/stx/debsentry.py
@@ -18,28 +18,38 @@ import pickle
 
 
 def get_subdebs(clue, package, logger):
-    if not os.path.exists(clue):
-        logger.warn("debs_entry:debs clue %s does not exist" % clue)
-        return None
-
-    with open(clue, 'rb') as fclue:
-        debs = pickle.load(fclue)
-        if package in debs.keys():
-            return debs[package]
-        return None
+    try:
+        with open(clue, 'rb') as fclue:
+            try:
+                debs = pickle.load(fclue)
+                if package in debs.keys():
+                    return debs[package]
+            except (EOFError, ValueError, AttributeError, ImportError, IndexError, pickle.UnpicklingError) as e:
+                logger.warn(f"debs_entry:failed to load {clue}, return None")
+    except IOError:
+        logger.warn(f"debs_entry:{clue} does not exist")
+    return None
 
 
 def set_subdebs(clue, package, debs, logger):
     debmap = {}
-    if os.path.exists(clue):
+    try:
         with open(clue, 'rb') as fclue:
-            debmap = pickle.load(fclue)
-            logger.debug("debs_entry:loaded the debs clue %s" % clue)
-    else:
-        logger.debug("debs_entry:%s does not exist" % clue)
+            try:
+                debmap = pickle.load(fclue)
+                logger.debug(f"debs_entry:loaded the debs clue {clue}")
+            except (EOFError, ValueError, AttributeError, ImportError, IndexError, pickle.UnpicklingError) as e:
+                logger.warn(f"debs_entry:failed to load {clue}, recreate it")
+                os.remove(clue)
+                debmap = {}
+    except IOError:
+        logger.debug(f"debs_entry:{clue} does not exist")
 
     debmap[package] = debs
-    with open(clue, 'wb+') as fclue:
-        pickle.dump(debmap, fclue, pickle.HIGHEST_PROTOCOL)
+    try:
+        with open(clue, 'wb+') as fclue:
+            pickle.dump(debmap, fclue, pickle.HIGHEST_PROTOCOL)
+    except IOError:
+        raise Exception(f"debs_entry:failed to write {clue}")
 
     return True
